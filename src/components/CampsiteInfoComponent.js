8fbfd2a9f7b1ff6b6670c14cbc8c0a6c7b5dc6e8
Phillip Erickson <phanter14c@gmail.com>
2021-03-26 14:28:46 -0400

    Combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import Button from "reactstrap/lib/Button";
+import { LocalForm, Control, Errors } from "react-redux-form";
+
+
 
  function RenderCampsite({campsite}) {
     return(
@@ -33,14 +37,105 @@ function RenderComments({comments}) {
                         </div>
                     );
                 })}
+                <CommentForm />
                 </div>
         );
     }
     else{
-        return <div>No comments available</div>;
+        return <div>No comments available <CommentForm /></div>;
     }
 }
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isModalOpen: false,
+        };
+      }
     
+      toggleModal = () => {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen,
+        });
+      };
+    
+      handleSubmit = (values) => {
+        this.toggleModal();
+        console.log(values);
+      };
+
+      render() {
+        return (
+            <div>
+            <Button onClick={this.toggleModal} className="fa fa-pencil fa-lg" outline>
+            Submit Comment
+            </Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+              <LocalForm onSubmit={(valuesObj) => this.handleSubmit(valuesObj)}>
+                <div className="form-group">
+                  <label htmlFor="rating">Rating</label>
+                  <Control.select
+                    model=".rating"
+                    id="rating"
+                    name="rating"
+                    className="form-control"
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Control.select>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="author">Your name</label>
+                  <Control.text
+                    model=".author"
+                    id="author"
+                    name="author"
+                    className="form-control"
+                    placeholder="Your name"
+                    validators={{
+                        required: (val) => val && val.length > 0,
+                        minLength: (val) => val && val.length >= 2,
+                        maxLength: (val) => val && val.length <= 15,
+                      }}
+                  />
+                  <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be at least 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="comment">Comment</label>
+                  <Control.textarea
+                    model=".comment"
+                    id="comment"
+                    name="comment"
+                    className="form-control"
+                    rows="6"
+                  />
+                </div>
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
+              </LocalForm>
+            </ModalBody>
+          </Modal>
+        </div>
+        );
+    }
+  }
 
 
 function CampsiteInfoComponent(props) {
