4a9a4c0094cadcc9a7a1e47d15efc019b30e9297
Phillip Erickson <phanter14c@gmail.com>
2021-03-26 18:06:19 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ import { baseUrl } from '../shared/baseUrl';
     
     );
 }
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments){
         return(
             <div className="col-md-5 m-1">
@@ -39,7 +39,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
         );
     }
@@ -64,7 +64,7 @@ class CommentForm extends Component {
     
       handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         
       };
 
@@ -180,7 +180,7 @@ if (props.campsite) {
                        <RenderCampsite campsite={props.campsite}/>
                        <RenderComments 
                           comments={props.comments}
-                          addComment={props.addComment}
+                          postComment={props.postComment}
                           campsiteId={props.campsite.id} />
                     </div>
                  </div>      
